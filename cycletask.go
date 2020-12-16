7a35d68296ea45b9e7d37fc28662f327dbca12e5
Wong <WongJSnow@gmail.com>
2020-12-16 18:48:53 +0800

    尝试修复内存异常

--- cycletask.go
+++ cycletask.go
@@ -180,7 +180,5 @@ func (c *CycleTaskUnit) Forward() {
 		log.Println("Tag", c.Tag, err)
 		return
 	}
-	if res_res == nil {
-		log.Println("Tag", c.Tag, "res_res is nil")
-	}
+	defer res_res.Body.Close()
 }
